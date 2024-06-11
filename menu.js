91481fa304305a1919a6908359730d6ddcffde1b
Marinn4 <160951587+Marinn4@users.noreply.github.com>
2024-06-11 04:00:48 +0000

    vai mn

--- menu.js
+++ menu.js
@@ -2,5 +2,6 @@ const btnMobile = document.getElementById('btn-mobi');
  
 function toggleMenu() {
     const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
 }
 btnMobile.addEventListener ('click', toggleMenu);
